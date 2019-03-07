859a3f1622a95957e1e39353df54e3c03a96fbc0
Dave Lee <davelee.com@gmail.com>
2019-03-07 00:16:59 -0800

    Add a starter accessor

--- include/ObjectFile.h
+++ include/ObjectFile.h
@@ -11,6 +11,8 @@ typedef struct ObjectFile ObjectFile;
 ObjectFile *binj_createObjectFile(const char *path);
 void binj_destroyObjectFile(ObjectFile *);
 
+const char *binj_archName(ObjectFile *);
+
 #ifdef __cplusplus
 }
 #endif
--- lib/ObjectFile.cpp
+++ lib/ObjectFile.cpp
@@ -19,3 +19,18 @@ struct ObjectFile {
 void binj_destroyObjectFile(::ObjectFile *objectFile) {
   delete objectFile;
 }
+
+const char *binj_archName(::ObjectFile *objectFile) {
+  switch (objectFile->real.getBinary()->getArch()) {
+  case Triple::ArchType::arm:
+    return "arm";
+  case Triple::ArchType::aarch64:
+    return "arm64";
+  case Triple::ArchType::x86:
+    return "x86";
+  case Triple::ArchType::x86_64:
+    return "x86_64";
+  default:
+    return "unknown";
+  }
+}
